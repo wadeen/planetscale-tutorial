1df63f068860f472744a63bc46bff5bce245b5aa
wadeen <meister.vvdo@gmail.com>
2023-03-11 14:15:56 +0900

    Add: postとgetの制御追加

---
+++ pages/api/authors/index.ts
@@ -0,0 +1,29 @@
+import { Author } from "@prisma/client";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Author[] | Author>) {
+  const { method } = req;
+
+  switch (method) {
+    case "GET":
+      const authors = await prisma.author.findMany();
+      res.status(200).json(authors);
+      break;
+
+    case "POST":
+      const author = await prisma.author.create({
+        data: {
+          name: "King nob",
+        },
+      });
+      res.status(200).json(author); // idを含む保存したデータを返す
+      break;
+
+    default:
+      res.setHeader("Allow", ["GET", "POST"]);
+      res.status(405).end(`Method ${method} Not Allowed`);
+  }
+}
